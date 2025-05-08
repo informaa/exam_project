0d8bf9c18a1ac9ba64333464ecd6ac633f368948
informaa <d.png@bk.ru>
2025-05-08 21:37:15 +0500

    test

--- src/main/java/com/astanait/universityschedule/config/SecurityConfig.java
+++ src/main/java/com/astanait/universityschedule/config/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
                         .requestMatchers("/schedule", "/exams").authenticated()
