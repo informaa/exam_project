db08a3be2d81160252b8d133fc61f493a03d10c8
informaa <d.png@bk.ru>
2025-05-08 18:51:54 +0500

    Initial commit: Spring Boot project structure created

---
+++ src/main/java/com/astanait/universityschedule/UniversityScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.astanait.universityschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astanait/universityschedule/UniversityScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astanait.universityschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
