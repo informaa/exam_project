fceab8c7dba457f175ec6d77982735fadca9d781
informaa <d.png@bk.ru>
2025-05-08 19:36:48 +0500

    test

--- src/main/java/com/astanait/universityschedule/RoleRepository/UserRepository.java
+++ src/main/java/com/astanait/universityschedule/RoleRepository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.astanait.universityschedule.RoleRepository;
 
-public class UserRepository {
+import com.astanait.universityschedule.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
 }
